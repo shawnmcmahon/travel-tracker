87cd856f0a88daf2932719f49245f532bb2c860c
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-23 18:18:07 -0600

    Begin testing files

--- src/destination.js
+++ src/destination.js
@@ -1,16 +1,16 @@
-import User from './user';
-import Trip from './trip';
-
-class Destination() {
-  constructor(property) {
-    this.destinationID = property.destinationID;
-    this.destinationLocation = property.destinationLocation;
-    this.destinationFlightCost = property.destinationFlightCost;
-    this.destinationImage = property.destinationImage;
-    this.destinationAlt = property.destinationAlt
-  }
-}
-
-
-
-export default Destination
+// import User from './user';
+// import Trip from './trip';
+//
+// class Destination() {
+//   constructor(property) {
+//     this.destinationID = property.destinationID;
+//     this.destinationLocation = property.destinationLocation;
+//     this.destinationFlightCost = property.destinationFlightCost;
+//     this.destinationImage = property.destinationImage;
+//     this.destinationAlt = property.destinationAlt
+//   }
+// }
+//
+//
+//
+// export default Destination
--- src/tripRepo.js
+++ src/tripRepo.js
@@ -1,10 +1,27 @@
 import User from './user';
 import Trip from './trip';
 
+import sampleTripData from './data/sampleTripData.js';
+
 class TripRepo {
-  constructor(tripData) {
-    this.tripData = tripData;
+  constructor(sampleTripData) {
+    this.tripData = sampleTripData;
   }
 }
+
+
+
+
+
+
+
+
+
+
+//find User's trips
+
+//find userExpensesYTD(userID, destination)
+
+
 
 export default TripRepo;

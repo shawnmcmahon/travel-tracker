220ab236f39d20e4b3ca1ab21bc4319787b711ab
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-23 19:28:12 -0600

    Add traveler property tests

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,29 +1,35 @@
 import { expect } from 'chai';
 
 import Traveler from '../src/traveler.js'
-import sampleUserData from '../src/data/sampleTravelerData.js'
+import sampleTravelerData from '../src/data/sampleTravelerData.js'
 
 let sampleTraveler;
 
-describe('User', () => {
+describe('Traveler', () => {
   let traveler1, traveler2, traveler3;
 
   beforeEach(() => {
 
-    traveler1 =  new Traveler(sampleTraveler.travelers[0])
+    traveler1 =  new Traveler(sampleTravelerData.travelers[0])
   });
 
   it('Should be a function', () => {
     expect(Traveler).to.be.a('function');
   })
 
-  it('Should have a user ID', () => {
-    //console.log(sampleUserData.travelers[0].id)
-    //console.log(user1.userID);
-    expect(traveler1.userID).to.eql(1);
+  it('Should have a traveler ID', () => {
+    expect(traveler1.id).to.eql(1);
   })
 
-  // it('Should')
+  it('Should have a traveler name', () => {
+    console.log(traveler1);
+    expect(traveler1.name).to.eql('Ham Leadbeater')
+  })
+
+  it('Should have a traveler type', () => {
+    console.log(traveler1);
+    expect(traveler1.travelerType).to.eql('relaxer')
+  })
 
 
 })

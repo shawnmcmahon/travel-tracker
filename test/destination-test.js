e9c71f098ef98d1068a669fa9f130297b1a0b4ef
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-24 14:33:37 -0600

    Begin destination testing

---
+++ test/destination-test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+
+import Destination from '../src/Destination';
+import destinationData from '../src/data/sampleDestinationData.js'
+
+
+describe('Destination', () => {
+  let destinationTest;
+
+
+  beforeEach(() => {
+
+    destinationTest = new Destination(destinationData);
+
+  })
+
+  
+
+})

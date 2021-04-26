0dc89154bb71a43959a36ead40c51d53498ca33d
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-04-25 18:58:32 -0600

    Add more dom styling

--- src/tripRepo.js
+++ src/tripRepo.js
@@ -1,26 +1,35 @@
-
 import Trip from './trip';
 
 
-
 class TripRepo {
-  constructor(tripData) {
+  constructor(tripData, destinationData) {
     this.tripData = tripData;
-    //console.log(tripData);
+    this.destinationData = destinationData;
+  }//console.log(tripData);
+
+
+  findUsersTrips(userID) {
+    //console.log(this.tripData)
+    const userTrips = this.tripData['trips'].filter(trip => trip.userID === userID)
+    //console.log(userTrips)
+    return userTrips
+
   }
+
+  calculateTripCost() {
+    //iterate through destinationData. if destination ID matches an ID within
+    //trip Data, use the tripdata.duration to make a calcuation.
+
+
+  }
+
+
 }
 
-
-
-//instiate trips data on this tripRepo
-
-
-
-
-
-//find User's trips
-
 //Destionations visited
+
+
+
 
 //find userExpensesYTD(userID, destination)
 
